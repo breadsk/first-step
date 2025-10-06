fec6ae9af75d858e919484ea563e4edfea27be4e
Nicolas <nicolas.programador@gmail.com>
2025-10-05 22:19:44 -0300

    Item counter

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,47 +1,18 @@
-import type { CSSProperties } from "react";
 
-
-//const favoriteGames:string[] = ['megaman' , 'suikoden' , 'final fantasy' ]
-const isActive = true;
-const persona = {
-    firstName:'Nicolas',
-    lastName:'Caceres',
-    age:42
-  }
-
-const myStyles: CSSProperties = {
-            backgroundColor: 'red',
-            color: isActive ? 'white' : 'yellow',
-            borderRadius: 2,
-            padding: 10,
-            fontWeight: 'bold'
-          }
-
+import { ItemCounter } from "./shopping-cart/itemCounter"
 
 export const FirstStepsApp = () => {
 
     
-
   return (
-    <>
-        {/* <p><b>{favoriteGames.join(', ')}</b></p> */}
-
-        {/* {favoriteGames.map((game, i) => (
-            <h2 key={i}>{game}</h2>
-        ))} */}
-
-        {/* <h2>{  isActive.toString() }</h2> */}
-        {/* <h2>{persona.toString()}</h2> */}
-        <h1>{    isActive ? 'Activo' : 'No activo' }</h1>
-
-        <h2>{JSON.stringify(persona)}</h2>
-
-
-        <p
-          style={myStyles}
-        >
-          Hola mundo
-        </p>
+    <>        
+        <h1>Carrito de Compras</h1>
+        
+        <ItemCounter />
+        <ItemCounter />
+        <ItemCounter />
+        
+        
     </>
   )
 }
---
+++ src/shopping-cart/ItemCounter.tsx
@@ -0,0 +1,22 @@
+import type { CSSProperties } from "react"
+
+const styles:CSSProperties = {
+        display: 'flex',
+        alignItems: 'center',
+        gap: 10,
+        marginTop: 10,
+    }
+
+export const ItemCounter = () => {
+  return (
+    <section style={styles}>
+        <span style={{
+            width: 150,
+
+        }}>Nintendo Switch 2</span>
+        <button>+1</button>
+        <span>10</span>
+        <button>-1</button>
+    </section>
+  )
+}
