433cb20f4df6f28b71f7c7369cb9b86ea474cc10
Nicolas <nicolas.programador@gmail.com>
2025-10-05 23:24:07 -0300

    Logica en el click

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,16 +1,36 @@
 
 import { ItemCounter } from "./shopping-cart/itemCounter"
 
+interface ItemInCart {
+  productName: string;
+  quantity:number;
+}
+
+const itemsInCart:ItemInCart[] = [
+  { productName: 'Nintendo Switch 2', quantity: 2 },
+  { productName: 'Play Station 5', quantity: 3 },
+  { productName: 'Game Boy', quantity: 4 },
+]
+
 export const FirstStepsApp = () => {
 
     
   return (
     <>        
         <h1>Carrito de Compras</h1>
+
+        {
+          itemsInCart.map( ({productName , quantity}) => (
+              <ItemCounter
+                key={productName}
+                productName={productName} 
+                quantity={quantity} />
+          ))
+        }
         
-        <ItemCounter itemName="Nintendo switch 2" quantity={2}/>
+        {/* <ItemCounter itemName="Nintendo switch 2" quantity={2}/>
         <ItemCounter itemName="Play station 5" quantity={3}/>
-        <ItemCounter itemName="Game Boy" quantity={4}/>
+        <ItemCounter itemName="Game Boy" quantity={4}/> */}
         
         
     </>
--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -8,22 +8,31 @@ const styles:CSSProperties = {
     }
 
 interface Props {
-    itemName:string;
+    key:string;
+    productName:string;
     quantity?:number; // number | undefined
 }
 
 
-export const ItemCounter = ({itemName,quantity}:Props) => {
+export const ItemCounter = ({productName,quantity}:Props) => {
+    
+    const handleClick = () => {
+        console.log(`Click en ${productName}`);
+    }
 
-    console.log(itemName);
 
   return (
     <section style={styles}>
         <span style={{
             width: 150,
 
-        }}>Nintendo Switch 2</span>
-        <button>+1</button>
+        }}>{productName}</span>
+        <button
+            // onMouseEnter={() => {
+            //     console.log(`Mouse enter ${productName}`);
+            // }}
+            onClick={handleClick}
+        >+1</button>
         <span>{quantity}</span>
         <button>-1</button>
     </section>
