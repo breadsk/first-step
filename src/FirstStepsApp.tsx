d3b6f135202f1d3ec8961865144838ce57f21f0f
Nicolas <nicolas.programador@gmail.com>
2025-10-05 22:03:58 -0300

    Uso de css

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,12 +1,22 @@
+import type { CSSProperties } from "react";
 
 
-const favoriteGames = ['megaman' , 'suikoden' , 'final fantasy' ]
-    const isActive = true;
-    const persona = {
-        firstName:'Nicolas',
-        lastName:'Caceres',
-        age:42
-     }
+//const favoriteGames:string[] = ['megaman' , 'suikoden' , 'final fantasy' ]
+const isActive = true;
+const persona = {
+    firstName:'Nicolas',
+    lastName:'Caceres',
+    age:42
+  }
+
+const myStyles: CSSProperties = {
+            backgroundColor: 'red',
+            color: isActive ? 'white' : 'yellow',
+            borderRadius: 2,
+            padding: 10,
+            fontWeight: 'bold'
+          }
+
 
 export const FirstStepsApp = () => {
 
@@ -14,17 +24,24 @@ export const FirstStepsApp = () => {
 
   return (
     <>
-        <p><b>{favoriteGames.join(', ')}</b></p>
+        {/* <p><b>{favoriteGames.join(', ')}</b></p> */}
 
-        {favoriteGames.map((game, i) => (
+        {/* {favoriteGames.map((game, i) => (
             <h2 key={i}>{game}</h2>
-        ))}
+        ))} */}
 
         {/* <h2>{  isActive.toString() }</h2> */}
         {/* <h2>{persona.toString()}</h2> */}
         <h1>{    isActive ? 'Activo' : 'No activo' }</h1>
 
         <h2>{JSON.stringify(persona)}</h2>
+
+
+        <p
+          style={myStyles}
+        >
+          Hola mundo
+        </p>
     </>
   )
 }
